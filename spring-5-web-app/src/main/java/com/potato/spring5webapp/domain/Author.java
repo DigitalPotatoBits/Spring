9e1acb3f3a41b6c018bbdaffb340381ff9ada85c
Joshua Baker <thepotatoprogrammer@gmail.com>
2021-03-19 21:32:17 -0500

    Added many to many relation between authors and books.

--- spring-5-web-app/src/main/java/com/potato/spring5webapp/domain/Author.java
+++ spring-5-web-app/src/main/java/com/potato/spring5webapp/domain/Author.java
@@ -1,20 +1,36 @@
 package com.potato.spring5webapp.domain;
 
+import javax.persistence.*;
 import java.util.Set;
 
+@Entity
 public class Author {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
     private String firstName;
     private String lastName;
+
+    @ManyToMany(mappedBy = "authors")
     private Set<Book> books;
 
     // JPA requires a zero arg constructor.
-    public Author() {
-    }
+    public Author() { }
 
     public Author(String firstName, String lastName, Set<Book> books) {
         this.firstName = firstName;
         this.lastName = lastName;
         this.books = books;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
     }
 
     public String getFirstName() {
--- spring-5-web-app/src/main/java/com/potato/spring5webapp/domain/Book.java
+++ spring-5-web-app/src/main/java/com/potato/spring5webapp/domain/Book.java
@@ -1,20 +1,39 @@
 package com.potato.spring5webapp.domain;
 
+import javax.persistence.*;
 import java.util.Set;
 
+@Entity
 public class Book {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
     private String title;
     private String isbn;
+
+    @ManyToMany
+    @JoinTable(name = "author_book",
+                joinColumns = @JoinColumn(name = "book_id"),
+                inverseJoinColumns = @JoinColumn(name = "author_id"))
     private Set<Author> authors;
 
     // JPA requires a zero arg constructor.
-    public Book() {
-    }
+    public Book() { }
 
     public Book(String title, String isbn, Set<Author> authors) {
         this.title = title;
         this.isbn = isbn;
         this.authors = authors;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
     }
 
     public String getTitle() {
